aaf7be78c37d8623183ef3d8cf486818039c4683
suu2589singh-ship-it <suu2589singh@gmail.com>
2026-01-04 21:48:32 +0530

    Update UserRoute.js

--- backEnd/UserRouter/UserRoute.js
+++ backEnd/UserRouter/UserRoute.js
@@ -1,18 +1,18 @@
 const express=require("express")
 const userlogin=require("../UserControler/Userdata")
-const Register=require("../UserControler/Register")
+const register=require("../UserControler/Register")
 const sendMessage=require("../UserControler/Massage")
 const checkauth = require("../UserControler/Auth")
-const logout = require("../UserControler/Logout.")
-const SaveData=require("../UserControler/Alldata")
+const logout = require("../UserControler/Logout")
+const saveData=require("../UserControler/Alldata")
 const Routers=express.Router()
 
 Routers.post("/login",userlogin)
-Routers.post("/Register",Register)
+Routers.post("/Register",register)
 Routers.post("/massage",sendMessage)
 Routers.get("/checkauth",checkauth)
 Routers.post("/logout",logout)
-Routers.post("/Savedata",SaveData)
+Routers.post("/Savedata",saveData)
 
 
 
